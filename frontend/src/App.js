2b1f718c4b138b9a1947fa91395aeba93a3f0cb8
Una Liu <144079763+aaaaaaaanO@users.noreply.github.com>
2024-10-04 21:08:39 -0400

    working fine

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 // import the ChatBox component; change the chatbot file to your own!! 
 // make sure to save the iterations
-import ChatBox from './components/kevin_gpt3';
+import ChatBox from './components/kevin_gpt3_debug';
 import './App.css';
 
 const App = () => {
